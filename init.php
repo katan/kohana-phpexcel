d6076da61af8531ad1189eb3a79fd512752cc38f
kevin <kai.yuan@foxmail.com>
2012-12-12 14:45:17 +0800

    add PHPExcel

--- init.php
+++ init.php
@@ -1,4 +1,4 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 // Autoloading for PHPExcel
-require Kohana::find_file('vendor/phpexcel', 'PHPExcel');
+require Kohana::find_file('vendor/PHPExcel/Classes', 'PHPExcel');
