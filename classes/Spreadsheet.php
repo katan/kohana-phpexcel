bf1431a30cb04acab30bc365908cb8359bfef4ea
kevin <kai.yuan@foxmail.com>
2012-12-14 01:45:17 +0800

    make compatible with the version 3.3

---
+++ classes/Spreadsheet.php
@@ -0,0 +1,18 @@
+<?php
+
+defined('SYSPATH') or die('No direct access allowed.');
+
+/**
+ * PHP Excel library. Helper class to make and read spreadsheet easier
+ * 
+ * @package Koahana
+ * @category spreadsheet
+ * @author Katan
+ * @license    http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt    LGPL
+ * 
+ * @see https://github.com/rafsoaken/kohana-phpexcel (Flynsarmy, Dmitry Shovchko)
+ * 
+ */
+class Spreadsheet extends Kohana_Spreadsheet {
+    
+}
